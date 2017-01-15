e12e44d71fccf0c09be4d50fb86ff7c9f0c0c855
xan.brian <xuan_bingan@hotmail.com>
2017-01-15 17:19:05 +0800

    up

--- Source/Common/Utils/Parameters.cs
+++ Source/Common/Utils/Parameters.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Insight.Utils.Common;
 using Insight.Utils.Entity;
 using Insight.WS.Server.Common.Entity;
 
@@ -6,16 +9,49 @@ namespace Insight.WS.Server.Common.Utils
 {
     public static class Parameters
     {
+        private static List<FileInfo> _FileList;
+        private static DateTime _ReadTime;
+
         // 当前连接基础应用服务器
-        public static string BaseServer;
+        public static string BaseServer = Util.GetAppSetting("BaseServer");
 
         // 验证接口URL
-        public static string VerifyUrl;
+        public static string VerifyUrl = $"{BaseServer}/security/v1.0/tokens/verify";
 
         // 数据库连接字符串
         public static string Database = new Entities().Database.Connection.ConnectionString;
 
-        // 客户端文件列表
-        public static List<FileInfo> FileList;
+        /// <summary>
+        /// 客户端文件列表
+        /// </summary>
+        public static List<FileInfo> FileList
+        {
+            get
+            {
+                var span = DateTime.Now - _ReadTime;
+                if (span.TotalMinutes < 30 && _FileList != null) return _FileList;
+
+                _ReadTime = DateTime.Now;
+                Util.GetLocalFiles(_FileList, $"{Environment.CurrentDirectory}\\Client", ".dll|.exe|.frl");
+                return _FileList;
+            }
+        }
+
+        /// <summary>
+        /// 可用服务列表
+        /// </summary>
+        public static List<SYS_Interface> Services => GetServices();
+
+        /// <summary>
+        /// 读取数据库中的服务信息
+        /// </summary>
+        /// <returns></returns>
+        private static List<SYS_Interface> GetServices()
+        {
+            using (var context = new Entities())
+            {
+                return context.SYS_Interface.ToList();
+            }
+        }
     }
 }
--- Source/InsightServer/InsightServer.cs
+++ Source/InsightServer/InsightServer.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Timers;
-using System.Windows.Forms;
-using Insight.Utils.Entity;
+﻿using System.ServiceProcess;
 using Insight.WCF;
-using Insight.WS.Server.Common.Entity;
 using Insight.WS.Server.Common.Utils;
 using static Insight.Utils.Common.Util;
 
@@ -13,92 +7,44 @@ namespace Insight.WS.Server
 {
     public partial class InsightServer : ServiceBase
     {
-
-        /// <summary>
-        /// 运行中的服务主机
-        /// </summary>
-        private static Service Services;
-
-        /// <summary>
-        /// 服务根路径
-        /// </summary>
-        private static readonly string RootPath = Application.StartupPath + "\\Client";
-
-        #region 构造函数
+        private static Service _Services;
 
         public InsightServer()
         {
             InitializeComponent();
-            InitSeting();
-
-            // 每小时更新一次服务器文件列表
-            var updateinfo = new System.Timers.Timer(3600000);
-            updateinfo.Elapsed += UpdateFileList;
-            updateinfo.Enabled = true;
         }
 
-        #endregion
-
-        #region 服务行为
-
+        /// <summary>
+        /// 启动WCF服务主机
+        /// </summary>
+        /// <param name="args"></param>
         protected override void OnStart(string[] args)
         {
-            // 启动WCF服务主机
-            Services = new Service();
-            using (var context = new Entities())
+            _Services = new Service();
+            foreach (var info in Parameters.Services)
             {
-                var list = context.SYS_Interface.ToList();
-                foreach (var info in list)
+                var service = new Service.Info
                 {
-                    var service = new Service.Info
-                    {
-                        BaseAddress = GetAppSetting("Address"),
-                        Port = info.Port,
-                        Path = info.Path,
-                        Version = info.Version,
-                        NameSpace = info.NameSpace,
-                        Interface = info.Interface,
-                        ComplyType = info.Service,
-                        ServiceFile = info.ServiceFile
-                    };
-                    Services.CreateHost(service);
-                }
+                    BaseAddress = GetAppSetting("Address"),
+                    Port = info.Port,
+                    Path = info.Path,
+                    Version = info.Version,
+                    NameSpace = info.NameSpace,
+                    Interface = info.Interface,
+                    ComplyType = info.Service,
+                    ServiceFile = info.ServiceFile
+                };
+                _Services.CreateHost(service);
             }
-            Services.StartService();
+            _Services.StartService();
         }
 
+        /// <summary>
+        /// 停止服务
+        /// </summary>
         protected override void OnStop()
         {
-            Services.StopService();
+            _Services.StopService();
         }
-
-        #endregion
-
-        #region 私有方法
-
-        /// <summary>
-        /// 初始化环境变量
-        /// </summary>
-        private void InitSeting()
-        {
-            UpdateFileList(null, null);
-            Parameters.BaseServer = GetAppSetting("BaseServer");
-            Parameters.VerifyUrl = $"{Parameters.BaseServer}/security/v1.0/tokens/verify";
-        }
-
-        /// <summary>
-        /// 更新服务器文件列表
-        /// </summary>
-        /// <param name="source"></param>
-        /// <param name="e"></param>
-        private void UpdateFileList(object source, ElapsedEventArgs e)
-        {
-            var list = new List<FileInfo>();
-            GetLocalFiles(list, RootPath, ".dll|.exe|.frl");
-            Parameters.FileList = list;
-        }
-
-        #endregion
-
     }
 }
